4179eeb746a7f08bc1ffd395d117226e2bf46aeb
Keshava Muraari <19950180+keshava19@users.noreply.github.com>
2019-03-03 17:35:23 +0530

    Added a floating action button menu
    Added FAB menu with animation controller, only clear button is there for now.

--- lib/draw_page.dart
+++ lib/draw_page.dart
@@ -1,13 +1,24 @@
 import 'package:draw/painter.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class DrawPage extends StatefulWidget {
   @override
   _DrawPageState createState() => _DrawPageState();
 }
 
-class _DrawPageState extends State<DrawPage> {
-  List<Offset> points = List<Offset>();
+class _DrawPageState extends State<DrawPage> with TickerProviderStateMixin {
+  AnimationController controller;
+  var points = List<Offset>();
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 250),
+      );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,17 +33,53 @@ class _DrawPageState extends State<DrawPage> {
               points = List.from(points)..add(localOffset);
             });
           },
-          onPanEnd: (DragEndDetails details) => points.add(null),
+          onPanEnd: (_) => points.add(null),
           child: CustomPaint(
             painter: Painter(points: points),
             size: Size.infinite,
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => points.clear(),
-        child: Icon(Icons.clear),
-      ),
+      floatingActionButton: Column(
+        mainAxisSize:MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            height: 70,
+            width: 56,
+            alignment: Alignment.topCenter,
+            child: ScaleTransition(
+              scale: CurvedAnimation(
+                parent: controller,
+                curve: Curves.easeInOutCubic
+              ),
+              child: FloatingActionButton(
+                child: const Icon(Icons.clear),
+                mini: true,
+                onPressed: () => points.clear(),
+              ),
+            )
+          ),
+          FloatingActionButton(
+            child: AnimatedBuilder(
+              animation: controller,
+              builder: (_, __) {
+                return Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.rotationZ(controller.value / 2 * math.pi),
+                  child: const Icon(Icons.brush),
+                  );
+              },
+            ),
+            onPressed: () {
+              if(controller.isDismissed) {
+                controller.forward();
+              } else {
+                controller.reverse();
+              }
+            },
+          ),
+        ],
+      )
     );
   }
 }
